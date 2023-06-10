f0c3444e8349c8990187f9028e53c2ace89aa62d
Nalinaka <nalinaka@gmail.com>
2023-06-09 21:09:02 -0700

    Completed assisngment but some code is broken

--- commands.js
+++ commands.js
@@ -20,6 +20,66 @@ db.collections.insertOne({
   captured: false
 })
 
+db.collections.insertOne ({
+          "name": "Lokinkajou",
+          "species": "Kinkajou",
+          "location": "Tropical rainforest",
+          "wantedFor": "Partying too late at night",
+          "client": "White tiger",
+          "reward": 1000,
+          "captured": false
+        }),
+
+db.collections.insertOne ({    
+          "name": "Nebullama",
+          "species": "Llama",
+          "location": "Grasslands",
+          "wantedFor": "Drinking all the water from the ocean",
+          "client": "Songbird",
+          "reward": 2500,
+          "captured": false
+        }),
+
+db.collections.insertOne ({
+          "name": "Polarwind",
+          "species": "Polar Bear",
+          "location": "Arctic",
+          "wantedFor": "Not hibernating",
+          "client": "Sabertooth",
+          "reward": 4000,
+          "captured": false
+        }),
+
+db.collections.insertOne ({
+          "name": "Wrecking Crows",
+          "species": "Crow",
+          "location": "Grasslands",
+          "wantedFor": "Cawing too loudly",
+          "client": "Red wolf",
+          "reward": 40000,
+          "captured": false
+        }),
+
+db.collections.insertOne ({
+          "name": "Grandhog",
+          "species": "Groundhog",
+          "location": "Woodlands",
+          "wantedFor": "Not coming out of the hole on time and prolonging winter",
+          "client": "Songbird",
+          "reward": 50000,
+          "captured": false
+        }),
+
+db.collections.insertOne ({
+          "name": "Grim Panda",
+          "species": "Giant Panda",
+          "location": "Temperate forest",
+          "wantedFor": "Eating all the bamboo",
+          "client": "Red wolf",
+          "reward": 5000,
+          "captured": false
+        })
+
 // 2. Query for all bounties in the bounties collection
 show collections
 
@@ -29,32 +89,40 @@ db.bounties.insertMany([])
 // MANAGE THE DATABASE
 // Queries
 // 1. Query for all bounties in the Grasslands
-db.bounties.find()
+db.bounties.find ({allbounties})
 
 // 2. Query for all bounties with a reward worth 10000 or more
-db.destinations.updateOne(
-    {
-        "name": "Scavengers",
-        "species": "animals",
-        "location": "Grasslands",
-        "wantedFor": "transportation broke down",
-        "client": "Scavengers",
-        "reward": 10000,
-        "captured": false
-      }
-)
+db.bounties.find ({reward: 10000 })
+
 
 // 3. Query for all bounties, but exclude the client attribute from being shown
-
+db.bounties.find ({client: 0}), ({captured: false}), 
+{species: Kinkajou, Lama, PolarBear, Crow, Groundhog, GiantPanda }
+    
 
 // 4. Query for a Groundhog in the Woodlands
-
+        db.inventory.find( {
+            $and: [
+                { $or: [ { location: { $lt : Woodlands } }, { name : { $gt: Groundhog } } ] },
+                { $or: [ { captured: false } }, price: { $lt : 50,000 }, { client : { $gt: Client } } ] }, 
+                    { wantedFor: "Not coming out of the hole on time and prolonging winter" }   
+         )
 
 // Update and Delete
 // 1. Update the reward for Polarwind to 10000
+db.bounties.insertOne ({name: Polarwind}), ({captured: false}), ({reward: 10000}),({species: PolarBear}) ({location: Arctic}),
+({wantedFor: Nothibernating}), ({client: Sabertooth})
+
 
 // 2. Remove Lokinkajou
+db.bounties.deleteOne({ species: "Lokinkajou"})
+
 
 // 3. Delete all bounties sent by Songbird
+db.bounties.deleteMany({ client: "Songbird"})
+
 
 // 4. Update all captured statuses to true
+db.bounties.find({ 
+    captured: true })
+
