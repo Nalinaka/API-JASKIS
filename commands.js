6c911daef9d19dc471f537ec66864f1c691eec6c
Nalinaka <nalinaka@gmail.com>
2023-06-10 10:19:51 -0700

    Fixed code

--- commands.js
+++ commands.js
@@ -4,84 +4,89 @@
 // GETTING STARTED
 // 1. Create a database called jaskis
 use jaskis
+//switches to DB jaskis
 
 // 2. Create a collection called bounties
 db.createCollection('bounties')
+show collections 
+bounties
 
 // ADD THE ANIMAL BOUNTIES
 // 1. Insert the given "Thanoceros" bounty object
-db.collections.insertOne({
-  name: "Thanoceros",
-  species: "Rhinoceros",
-  location: "Grasslands",
-  wantedFor: "Eating too much grass",
-  client: "Songbird",
-  reward: 10000,
-  captured: false
+        db.bounties.insertOne({
+        name: "Thanoceros",
+        species: "Rhinoceros",
+        location: "Grasslands",
+        wantedFor: "Eating too much grass",
+        client: "Songbird",
+        reward: 10000,
+        captured: false
 })
 
-db.collections.insertOne ({
-          "name": "Lokinkajou",
-          "species": "Kinkajou",
-          "location": "Tropical rainforest",
-          "wantedFor": "Partying too late at night",
-          "client": "White tiger",
-          "reward": 1000,
-          "captured": false
+// If I choose to do them all this is what the code looks like:
+
+        db.bounties.insertOne({
+          name: "Lokinkajou",
+          species: "Kinkajou",
+          location: "Tropical rainforest",
+          wantedFor: "Partying too late at night",
+          client: "White tiger",
+          reward: 1000,
+          captured: false
         }),
 
-db.collections.insertOne ({    
-          "name": "Nebullama",
-          "species": "Llama",
-          "location": "Grasslands",
-          "wantedFor": "Drinking all the water from the ocean",
-          "client": "Songbird",
-          "reward": 2500,
-          "captured": false
+        db.bounties.insertOne({  
+          name: "Nebullama",
+          species: "Llama",
+          location: "Grasslands",
+          wantedFor: "Drinking all the water from the ocean",
+          client: "Songbird",
+          reward: 2500,
+          captured: false
         }),
 
-db.collections.insertOne ({
-          "name": "Polarwind",
-          "species": "Polar Bear",
-          "location": "Arctic",
-          "wantedFor": "Not hibernating",
-          "client": "Sabertooth",
-          "reward": 4000,
-          "captured": false
+        db.bounties.insertOne({
+          name: "Polarwind",
+          species: "Polar Bear",
+          location: "Arctic",
+          wantedFor: "Not hibernating",
+          client: "Sabertooth",
+          reward: 4000,
+          captured: false
         }),
 
-db.collections.insertOne ({
-          "name": "Wrecking Crows",
-          "species": "Crow",
-          "location": "Grasslands",
-          "wantedFor": "Cawing too loudly",
-          "client": "Red wolf",
-          "reward": 40000,
-          "captured": false
+        db.bounties.insertOne({
+          name: "Wrecking Crows",
+          species: "Crow",
+          location: "Grasslands",
+          wantedFor: "Cawing too loudly",
+          client: "Red wolf",
+          reward: 40000,
+          captured: false
         }),
 
-db.collections.insertOne ({
-          "name": "Grandhog",
-          "species": "Groundhog",
-          "location": "Woodlands",
-          "wantedFor": "Not coming out of the hole on time and prolonging winter",
-          "client": "Songbird",
-          "reward": 50000,
-          "captured": false
+        db.bounties.insertOne({
+          name: "Grandhog",
+          species: "Groundhog",
+          location: "Woodlands",
+          wantedFor: "Not coming out of the hole on time and prolonging winter",
+          client: "Songbird",
+          reward: 50000,
+          captured: false
         }),
 
-db.collections.insertOne ({
-          "name": "Grim Panda",
-          "species": "Giant Panda",
-          "location": "Temperate forest",
-          "wantedFor": "Eating all the bamboo",
-          "client": "Red wolf",
-          "reward": 5000,
-          "captured": false
+        db.bounties.insertOne({
+          name: "Grim Panda",
+          species: "Giant Panda",
+          location: "Temperate forest",
+          wantedFor: "Eating all the bamboo",
+          client: "Red wolf",
+          reward: 5000,
+          captured: false
         })
 
 // 2. Query for all bounties in the bounties collection
-show collections
+db.bounties.find
 
 // 3. Insert many bounties at once using the given objects
 db.bounties.insertMany([])
@@ -89,33 +94,30 @@ db.bounties.insertMany([])
 // MANAGE THE DATABASE
 // Queries
 // 1. Query for all bounties in the Grasslands
-db.bounties.find ({allbounties})
+db.bounties.find ({location: "Grasslands"})
 
 // 2. Query for all bounties with a reward worth 10000 or more
-db.bounties.find ({reward: 10000 })
+db.bounties.find ({reward: { $gte: 10000 } })
 
 
 // 3. Query for all bounties, but exclude the client attribute from being shown
-db.bounties.find ({client: 0}), ({captured: false}), 
-{species: Kinkajou, Lama, PolarBear, Crow, Groundhog, GiantPanda }
-    
+db.bounties.find ({client: 0}), ({captured: false}),     
 
 // 4. Query for a Groundhog in the Woodlands
-        db.inventory.find( {
+        db.bounties.find( {
             $and: [
-                { $or: [ { location: { $lt : Woodlands } }, { name : { $gt: Groundhog } } ] },
-                { $or: [ { captured: false } }, price: { $lt : 50,000 }, { client : { $gt: Client } } ] }, 
-                    { wantedFor: "Not coming out of the hole on time and prolonging winter" }   
-         )
+             { species: "Groundhog" },
+             { location: "Woodlands" },
+
+            ]
+        })
 
 // Update and Delete
 // 1. Update the reward for Polarwind to 10000
-db.bounties.insertOne ({name: Polarwind}), ({captured: false}), ({reward: 10000}),({species: PolarBear}) ({location: Arctic}),
-({wantedFor: Nothibernating}), ({client: Sabertooth})
-
+db.bounties.findOneAndUpdate({name: Polarwind}, {$inc:{reward: 10000}})
 
 // 2. Remove Lokinkajou
-db.bounties.deleteOne({ species: "Lokinkajou"})
+db.bounties.findOneAnddelete({ name: "Lokinkajou"})
 
 
 // 3. Delete all bounties sent by Songbird
@@ -123,6 +125,5 @@ db.bounties.deleteMany({ client: "Songbird"})
 
 
 // 4. Update all captured statuses to true
-db.bounties.find({ 
-    captured: true })
+db.bounties.updateMany({captured: fasle }, {$set:{captured: true}})
 
